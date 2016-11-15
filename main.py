feee8742c7856f4c5576995a3382dd9852e58fb3
Kris Wagner <kewagner@andrew.cmu.edu>
2016-11-15 18:25:05 -0500

    Initial Commit
    Very basic functionality.
    - Generates new activity when browser page refreshed
    - Scales, but not quite down to mobile
    - Animated snow

---
+++ main.py
@@ -0,0 +1,33 @@
+"""`main` is the top level module for your Flask application."""
+
+# Import libraries
+import os
+import jinja2
+import webapp2
+import logging
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+# Import the Flask Framework
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    return template.render()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
